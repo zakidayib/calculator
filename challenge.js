e0f49f16c642e455604e2f8afc678d6d22ab0f0e
zaki tayyip <zakidayib1997@gmail.com>
2023-05-21 09:02:30 +0300

    calc challenge solved

--- challenge.js
+++ challenge.js
@@ -1,2 +1,22 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+let calculator = function(num1, num2, operator){
+    if (operator == "+"){
+        return num1 + num2;
+    }
+
+    else if (operator == "-"){
+        return num1 - num2;
+    }
+
+    else if (operator == "*"){
+        return num1 * num2;
+    }
+
+    else if (operator == "/"){
+        return num1 / num2;
+    }
+}
+
+console.log(calculator(5,3,"*"));
